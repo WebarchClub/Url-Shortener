361905b2d00e0fb557faedf01977adf58e34a80d
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-06-29 11:23:48 +0530

    final changes made

--- client/app.js
+++ client/app.js
@@ -1,29 +1,29 @@
-const formUrl = document.getElementById('urlForm');
-const inputUrl = document.getElementById('longUrl');
-const customId = document.getElementById('shortId');
-const urlList = document.getElementById('url-list');
-const customIdButton = document.getElementById('custom-btn');
-const errorBox = document.getElementById('error-box');
-const links = document.getElementsByTagName('a');
-const modalClose = document.getElementById('modal-close-button');
-const modalBox = document.getElementById('modal-box');
-const modalShortUrl = document.getElementById('modal-short-url');
-const copyBtn = document.getElementById('copy-button');
-const urlCheckbox = document.getElementById('url-checkbox');
+const formUrl = document.getElementById("urlForm");
+const inputUrl = document.getElementById("longUrl");
+const customId = document.getElementById("shortId");
+const urlList = document.getElementById("url-list");
+const customIdButton = document.getElementById("custom-btn");
+const errorBox = document.getElementById("error-box");
+const links = document.getElementsByTagName("a");
+const modalClose = document.getElementById("modal-close-button");
+const modalBox = document.getElementById("modal-box");
+const modalShortUrl = document.getElementById("modal-short-url");
+const copyBtn = document.getElementById("copy-button");
+const urlCheckbox = document.getElementById("uniqueID");
 
 // This server URL will change on deploy
 // let serverUrl = 'https://webarch-shortener.herokuapp.com';
-let serverUrl = 'http://localhost:5000';
+let serverUrl = "http://localhost:5000";
 
 // Helper function to add a URL item to the results
 const addUrltoList = (baseUrl, longUrl, shortUrl) => {
-  let li = document.createElement('li');
-  let shortUrlLink = document.createElement('a');
-  let longUrlLink = document.createElement('a');
-  let div1 = document.createElement('div');
-  let div2 = document.createElement('div');
+  let li = document.createElement("li");
+  let shortUrlLink = document.createElement("a");
+  let longUrlLink = document.createElement("a");
+  let div1 = document.createElement("div");
+  let div2 = document.createElement("div");
 
-  baseUrl = baseUrl + '/shorten';
+  baseUrl = baseUrl + "/shorten";
 
   let shortenedLongUrl;
 
@@ -38,41 +38,65 @@ const addUrltoList = (baseUrl, longUrl, shortUrl) => {
   longUrlLink.innerText = shortenedLongUrl;
   shortUrlLink.href = longUrl;
   longUrlLink.href = longUrl;
-  shortUrlLink.target = '_blank';
-  longUrlLink.target = '_blank';
+  shortUrlLink.target = "_blank";
+  longUrlLink.target = "_blank";
 
-  li.classList.add('url-list-item');
+  const urlListCopyBtn = document.createElement("button");
+
+  urlListCopyBtn.innerText = "Copy";
+  urlListCopyBtn.classList.add("copy-btn");
+
+  // console.log(urlListCopyBtn);
+
+  li.classList.add("url-list-item");
 
   div1.append(longUrlLink);
   div2.append(shortUrlLink);
 
   li.append(div1);
   li.append(div2);
+  li.append(urlListCopyBtn);
+
+  urlListCopyBtn.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    const area = document.createElement("textarea");
+
+    body.appendChild(area);
+    area.value = shortUrlLink.innerText;
+    area.select();
+    document.execCommand("copy");
+
+    urlListCopyBtn.innerText = "Copied";
+
+    body.removeChild(area);
+
+    console.log("area value", area.value);
+  });
 
   urlList.prepend(li);
 };
 
 const addErrorBox = (message) => {
-  errorBox.classList.remove('inactive');
+  errorBox.classList.remove("inactive");
   errorBox.innerText = message;
 };
 
 const removeErrorBox = () => {
-  errorBox.classList.add('inactive');
+  errorBox.classList.add("inactive");
 };
 
 const addHeadingsToList = () => {
-  const li = document.createElement('li');
-  const h1 = document.createElement('h3');
-  const h2 = document.createElement('h3');
+  const li = document.createElement("li");
+  const h1 = document.createElement("h3");
+  const h2 = document.createElement("h3");
 
-  h1.innerText = `Long URLs`;
-  h2.innerText = `Short URLs`;
+  h1.innerText = `Full URL`;
+  h2.innerText = `Short URL`;
 
   li.append(h1);
   li.append(h2);
-  li.classList.add('url-list-item');
-  li.id = 'url-heading';
+  // li.classList.add('url-list-item');
+  li.id = "url-heading";
 
   urlList.prepend(li);
 };
@@ -80,9 +104,9 @@ const addHeadingsToList = () => {
 // Edit input URL as https
 const editUrl = (inputUrl) => {
   inputUrl =
-    inputUrl.startsWith('https') === true
+    inputUrl.startsWith("https") === true
       ? inputUrl
-      : inputUrl.startsWith('www') === true
+      : inputUrl.startsWith("www") === true
       ? `https://${inputUrl.slice(4)}`
       : `https://${inputUrl}`;
 
@@ -99,7 +123,7 @@ const fetchAllShortUrls = async () => {
 
   let urls = data.data;
 
-  urlList.innerText = urls.length === 0 ? 'No URLs yet' : '';
+  urlList.innerText = urls.length === 0 ? "No URLs yet" : "";
 
   //  To get only the last 10 newly added URLs
   urls = urls.slice(Math.max(urls.length - 10, 0));
@@ -122,28 +146,28 @@ const createShortUrl = async (e) => {
   let longInputUrl = inputUrl.value;
 
   // check for url validation
-  if (longInputUrl === '') {
-    addErrorBox('Please add a URL, field cannot be empty');
+  if (longInputUrl === "") {
+    addErrorBox("Please add a URL, field cannot be empty");
   } else {
     // if there is an existing error, remove it
-    if (!errorBox.classList.contains('inactive')) {
+    if (!errorBox.classList.contains("inactive")) {
       removeErrorBox();
     }
 
-    if (modalBox.classList.contains('inactive')) {
-      modalBox.classList.remove('inactive');
+    if (modalBox.classList.contains("inactive")) {
+      modalBox.classList.remove("inactive");
     }
 
     console.log(urlList.innerText);
 
-    if (urlList.innerText.includes('No URLs yet')) {
-      urlList.innerText = '';
+    if (urlList.innerText.includes("No URLs yet")) {
+      urlList.innerText = "";
     } else {
-      const urlHeading = document.getElementById('url-heading');
+      const urlHeading = document.getElementById("url-heading");
       if (urlHeading !== null) urlHeading.remove();
     }
 
-    inputUrl.value = '';
+    inputUrl.value = "";
 
     longInputUrl = editUrl(longInputUrl);
 
@@ -151,7 +175,7 @@ const createShortUrl = async (e) => {
       longUrl: longInputUrl,
     };
 
-    if (customId.value !== null && customId.value !== '') {
+    if (customId.value !== null && customId.value !== "") {
       shortId = customId.value;
       reqBody.shortUrl = shortId;
     }
@@ -161,11 +185,11 @@ const createShortUrl = async (e) => {
 
     reqBody.isPrivate = isChecked;
 
-    customId.value = '';
+    customId.value = "";
     const response = await fetch(`${serverUrl}/shorten`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(reqBody),
     });
@@ -185,53 +209,55 @@ const createShortUrl = async (e) => {
   }
 };
 
+const copyLinkToClipboard = () => {
+  const r = document.createRange();
+  r.selectNode(modalShortUrl);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  copyBtn.innerText = "Copied";
+};
 // To remove the #section-id from url
 Array.prototype.forEach.call(links, function (elem, index) {
   //Get the hyperlink target and if it refers to an id go inside condition
-  const elemAttr = elem.getAttribute('href');
-  if (elemAttr && elemAttr.includes('#')) {
+  const elemAttr = elem.getAttribute("href");
+  if (elemAttr && elemAttr.includes("#")) {
     //Replace the regular action with a scrolling to target on click
-    elem.addEventListener('click', function (ev) {
+    elem.addEventListener("click", function (ev) {
       ev.preventDefault();
       //Scroll to the target element using replace() and regex to find the href's target id
-      document.getElementById(elemAttr.replace(/#/g, '')).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'nearest',
+      document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
       });
     });
   }
 });
 
 // Event Listeners
-formUrl.addEventListener('submit', createShortUrl);
-document.addEventListener('DOMContentLoaded', function () {
+formUrl.addEventListener("submit", createShortUrl);
+document.addEventListener("DOMContentLoaded", function () {
   fetchAllShortUrls();
 });
-customIdButton.addEventListener('click', (e) => {
+customIdButton.addEventListener("click", (e) => {
   e.preventDefault();
-  if (customId.classList.contains('inactive')) {
-    customId.classList.remove('inactive');
-    customIdButton.innerText = 'Hide alias input';
+  if (customId.classList.contains("inactive")) {
+    customId.classList.remove("inactive");
+    customIdButton.innerText = "Hide alias input";
   } else {
-    customId.classList.add('inactive');
-    customId.value = '';
-    customIdButton.innerText = 'Make custom alias';
+    customId.classList.add("inactive");
+    customId.value = "";
+    customIdButton.innerText = "Make custom alias";
   }
 });
-modalClose.addEventListener('click', function () {
-  if (!modalBox.classList.contains('inactive')) {
-    modalBox.classList.add('inactive');
-    modalShortUrl.innerText = '';
+modalClose.addEventListener("click", function () {
+  if (!modalBox.classList.contains("inactive")) {
+    modalBox.classList.add("inactive");
+    modalShortUrl.innerText = "";
   }
-  copyBtn.innerText = 'Copy';
+  copyBtn.innerText = "Copy";
 });
-copyBtn.addEventListener('click', function () {
-  const r = document.createRange();
-  r.selectNode(modalShortUrl);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(r);
-  document.execCommand('copy');
-  window.getSelection().removeAllRanges();
-  copyBtn.innerText = 'Copied';
-});
+copyBtn.addEventListener("click", copyLinkToClipboard);
+// urlListCopyBtn.addEventListener("click", copyLinkToClipboard);
